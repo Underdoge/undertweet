4c8665f1cdb5e1c2a05eb211ca62c35010a18bec
Eduardo Chapa <eduardo.chapa@gmail.com>
2022-10-26 18:09:13 -0500

    test

--- streamv2.js
+++ streamv2.js
@@ -198,130 +198,138 @@ exports.endStream = function() {
 
 exports.startStream = function(db) {
     //Get following rules
+    console.log('starting stream');
     needle.get(rulesURL, { headers: { "authorization": `Bearer ${token}`}}, function (error, response){
-        if (response.statusCode !== 200)
+        if (response.statusCode !== 200) {
             irc.sayToChannel('#testing',`Get All Rules Error Code:${response.statusCode} \n Error:${response.body}`);
-        if (!Array.isArray(response.body.data)) {
-            return null;
-        }
-        let ids = response.body.data.map(rule => rule.id);
-        let data = {
-            "delete": {
-                "ids": ids
-            }
-        }
-        //Delete following rules
-        needle.post(rulesURL, data, {headers: {"content-type": "application/json","authorization": `Bearer ${token}`}},function (error,response){
-            if (response.statusCode !== 200) {
-                irc.sayToChannel('#testing',`Delete All Rules Error Code:${response.statusCode} \n Error:${response.body}`);
+        } else { 
+            if (response.body.meta.result_count == 0) {
+                console.log("Nothing to delete: " + response.body);
+                ids = null
             } else {
-                db.find({}, function (err, following) {
-                    let following_nicks = [],
-                        following_rule = null;
-                    channels = [];
-                    following.forEach(function (channel,index) {
-                        channels.push([channel.channel]);
-                        channels[index].push(channel.handles);
-                        channel.handles.forEach(function (nick) {
-                            if (following_nicks.indexOf(nick) === -1)
-                                following_nicks.push(nick);
-                        });
-                    });
-                    if(following_nicks.length > 0){
-                        following_nicks.forEach(function(nick,i) {
-                            if (i === 0){
-                                following_rule = `from:${nick}`;
-                            } else {
-                                following_rule = following_rule + ` OR from:${nick}`;
-                            }
-                        });
-                        const rules = [{
-                            'value': following_rule,
-                            'tag': 'following nicks'
-                        }];
-                        const data = {
-                            "add": rules
-                        };
-                        //Set following rules
-                        needle.post(rulesURL, data, { headers: {"content-type": "application/json","authorization": `Bearer ${token}`}}, function (error,response){
-                            if (response.statusCode !== 201) {
-                                irc.sayToChannel('#testing',`Set Rules Error Code:${response.statusCode} \n Error:${response.body}`);
-                            } else {
-                                console.log(`Following rule set: ${following_rule}`);
-                                if (getStream()){
-                                    getStream().abort();
-                                    getPipe().destroy();
-                                    getStream().removeAllListeners();
-                                    getStream().destroy();
-                                }
-                                let
-                                    p = new streamReader(),          
-                                    stream = needle.get(streamURL, {
-                                        headers: {
-                                            "User-Agent": "v2FilterStreamJS",
-                                            "Authorization": `Bearer ${token}`
-                                        },
-                                        timeout: 20000
-                                    });
-                                stream.on('error', function (error) {
-                                    console.log(`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Error Code:${error.code} \n Error:${error}`);
-                                    irc.sayToChannel('#testing',`Error in connection: "${error.code}", restarting.`);
-                                    exports.startStream(new nedb(config.nedb));
-                                })
-                                .on('timeout', function() {
-                                    console.log(`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Connection Timeout.`);
-                                    irc.sayToChannel('#testing','Connection Timeout.');
-                                })
-                                .on('response', function(response) {
-                                    if (response.statusCode != 200){
-                                        irc.sayToChannel('#testing',`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Error in response, status code: ${response.statusCode}`);
-                                        console.log(`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Error in response, status code: ${response.statusCode}`);
-                                    } else {
-                                        irc.sayToChannel('#testing',`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Stream started. Response OK, status code: ${response.statusCode}`);
-                                        console.log(`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Stream started. Response OK, status code: ${response.statusCode}`);
-                                        setWait(60000);
-                                    }
-                                    setStatusCode(response.statusCode);
-                                })
-                                .on('end', function() {
-                                    if (getStatusCode() == 200) {
-                                        irc.sayToChannel('#testing',`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Connection ended, restarting.`);
-                                        console.log(`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Connection ended, restarting.`);
-                                        exports.startStream(new nedb(config.nedb));
-                                    } else
-                                    if (getStatusCode() == 406) {
-                                        irc.sayToChannel('#testing',`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Not following any accounts yet.`);
-                                        console.log(`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Not following any accounts yet.`);
-                                    } else
-                                    if (getStatusCode() == 420) {
-                                        irc.sayToChannel('#testing',`[${new Date().toLocaleTimeString('en-us', dateOptions)}] The client has connected too frequently or is reconnecting too fast. Retrying in ${getWait()/1000} seconds.`);
-                                        console.log(`[${new Date().toLocaleTimeString('en-us', dateOptions)}] The client has connected too frequently or is reconnecting too fast. Retrying in ${getWait()/1000} seconds.`);
-                                        setTimeout(function() { exports.startStream(new nedb(config.nedb));},getWait());
-                                        setWait(2*getWait());
-                                    } else
-                                    if (getStatusCode() == 503) {
-                                        irc.sayToChannel('#testing',`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Service Unavailable. A streaming server is temporarily overloaded. Retrying in 5 minutes`);
-                                        console.log(`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Service Unavailable. A streaming server is temporarily overloaded. Retrying in 5 minutes`);
-                                        setTimeout(function() { exports.startStream(new nedb(config.nedb));},300000);
-                                    } else
-                                    if (getStatusCode() == 500) {
-                                        irc.sayToChannel('#testing',`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Server Error 500. Retrying in ${getWait()/1000} seconds.`);
-                                        console.log(`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Server Error 500. Retrying in ${getWait()/1000} seconds.`);
-                                        setTimeout(function() { exports.startStream(new nedb(config.nedb));},getWait());
-                                        setWait(2*getWait());
-                                    } 
-            
-                                })
-                                .pipe(p);
-                                setPipe(p);
-                                setStream(stream);
-                            }
-                        });
-                    } else {
-                        irc.sayToChannel('#testing',`Not following anyone, no rules defined yet.`);
-                    }
-                });
+                console.log("Found some rules to delete");
+                var ids = response.body.data.map(rule => rule.id);
             }
-        });
+            let data = {
+                "delete": {
+                    "ids": ids
+                }
+            }
+            //Delete following rules
+            needle.post(rulesURL, data, {headers: {"content-type": "application/json","authorization": `Bearer ${token}`}},function (error,response){
+                if (response.statusCode !== 200) {
+                    irc.sayToChannel('#testing',`Delete All Rules Error Code:${response.statusCode}`); 
+                   console.log(`Error deleting rules:${response.body}`);
+                } else {
+                    db.find({}, function (err, following) {
+                        let following_nicks = [],
+                            following_rule = null;
+                        channels = [];
+                        following.forEach(function (channel,index) {
+                            channels.push([channel.channel]);
+                            channels[index].push(channel.handles);
+                            channel.handles.forEach(function (nick) {
+                                if (following_nicks.indexOf(nick) === -1)
+                                    following_nicks.push(nick);
+                            });
+                        });
+                        if(following_nicks.length > 0){
+                            following_nicks.forEach(function(nick,i) {
+                                if (i === 0){
+                                    following_rule = `from:${nick}`;
+                                } else {
+                                    following_rule = following_rule + ` OR from:${nick}`;
+                                }
+                            });
+                            const rules = [{
+                                'value': following_rule,
+                                'tag': 'following nicks'
+                            }];
+                            const data = {
+                                "add": rules
+                            };
+                            //Set following rules
+                            console.log('setting following rules');
+                            needle.post(rulesURL, data, { headers: {"content-type": "application/json","authorization": `Bearer ${token}`}}, function (error,response){
+                                if (response.statusCode !== 201) {
+                                    irc.sayToChannel('#testing',`Set Rules Error Code:${response.statusCode} \n Error:${response.body}`);
+                                } else {
+                                    console.log(`Following rule set: ${following_rule}`);
+                                    if (getStream()){
+                                        getStream().abort();
+                                        getPipe().destroy();
+                                        getStream().removeAllListeners();
+                                        getStream().destroy();
+                                    }
+                                    let
+                                        p = new streamReader(),          
+                                        stream = needle.get(streamURL, {
+                                            headers: {
+                                                "User-Agent": "v2FilterStreamJS",
+                                                "Authorization": `Bearer ${token}`
+                                            },
+                                            timeout: 20000
+                                        });
+                                    stream.on('error', function (error) {
+                                        console.log(`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Error Code:${error.code} \n Error:${error}`);
+                                        irc.sayToChannel('#testing',`Error in connection: "${error.code}", restarting.`);
+                                        exports.startStream(new nedb(config.nedb));
+                                    })
+                                    .on('timeout', function() {
+                                        console.log(`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Connection Timeout.`);
+                                        irc.sayToChannel('#testing','Connection Timeout.');
+                                    })
+                                    .on('response', function(response) {
+                                        if (response.statusCode != 200){
+                                            irc.sayToChannel('#testing',`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Error in response, status code: ${response.statusCode}`);
+                                            console.log(`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Error in response, status code: ${response.statusCode}`);
+                                        } else {
+                                            irc.sayToChannel('#testing',`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Stream started. Response OK, status code: ${response.statusCode}`);
+                                            console.log(`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Stream started. Response OK, status code: ${response.statusCode}`);
+                                            setWait(60000);
+                                        }
+                                        setStatusCode(response.statusCode);
+                                    })
+                                    .on('end', function() {
+                                        if (getStatusCode() == 200) {
+                                            irc.sayToChannel('#testing',`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Connection ended, restarting.`);
+                                            console.log(`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Connection ended, restarting.`);
+                                            exports.startStream(new nedb(config.nedb));
+                                        } else
+                                        if (getStatusCode() == 406) {
+                                            irc.sayToChannel('#testing',`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Not following any accounts yet.`);
+                                            console.log(`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Not following any accounts yet.`);
+                                        } else
+                                        if (getStatusCode() == 420) {
+                                            irc.sayToChannel('#testing',`[${new Date().toLocaleTimeString('en-us', dateOptions)}] The client has connected too frequently or is reconnecting too fast. Retrying in ${getWait()/1000} seconds.`);
+                                            console.log(`[${new Date().toLocaleTimeString('en-us', dateOptions)}] The client has connected too frequently or is reconnecting too fast. Retrying in ${getWait()/1000} seconds.`);
+                                            setTimeout(function() { exports.startStream(new nedb(config.nedb));},getWait());
+                                            setWait(2*getWait());
+                                        } else
+                                        if (getStatusCode() == 503) {
+                                            irc.sayToChannel('#testing',`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Service Unavailable. A streaming server is temporarily overloaded. Retrying in 5 minutes`);
+                                            console.log(`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Service Unavailable. A streaming server is temporarily overloaded. Retrying in 5 minutes`);
+                                            setTimeout(function() { exports.startStream(new nedb(config.nedb));},300000);
+                                        } else
+                                        if (getStatusCode() == 500) {
+                                            irc.sayToChannel('#testing',`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Server Error 500. Retrying in ${getWait()/1000} seconds.`);
+                                            console.log(`[${new Date().toLocaleTimeString('en-us', dateOptions)}] Server Error 500. Retrying in ${getWait()/1000} seconds.`);
+                                            setTimeout(function() { exports.startStream(new nedb(config.nedb));},getWait());
+                                            setWait(2*getWait());
+                                        } 
+                
+                                    })
+                                    .pipe(p);
+                                    setPipe(p);
+                                    setStream(stream);
+                                }
+                            });
+                        } else {
+                            irc.sayToChannel('#testing',`Not following anyone, no rules defined yet.`);
+                        }
+                    });
+                }
+            });
+        }
     });
 };
