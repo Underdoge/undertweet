2b62bba903712afa10c8c2dd738e119eef1e7654
Underdoge <eduardo.chapa@gmail.com>
2022-10-26 14:13:37 -0500

    ok

--- irc.js
+++ irc.js
@@ -1,17 +1,22 @@
 'use strict';
 
 const
+    joinImages = require("join-images"),
+    fs = require("fs"),
     packageInf = require('./package'),
     IRC = require('irc-framework'),
     colors = require('irc-colors'),
     config = require('./config'),
-	stream = require('./streamv2'),
+    stream = require('./streamv2'),
+    path = require('path'),
     nedb = require('nedb'),
     host = config.irc.host,
     port = config.irc.port,
     nick = config.irc.nick,
+    username = config.irc.nick,
+    gecos = config.irc.nick,
     tls = config.irc.tls,
-    pass = config.irc.pass,
+    password = config.irc.pass,
     dateOptions = {
         'timeZone':'America/Mexico_City',
         'weekday': 'long', 'year': 'numeric', 'month': 'short',
@@ -21,7 +26,8 @@ const
         '&amp;': '&', '&lt;':'<', '&gt;':'>',
     },
     htmlKeys = ['&amp;', '&lt;', '&gt;'],
-    token = config.twitter.bearer_token;
+    token = config.twitter.bearer_token,
+    channels = [];
 
 var
     needle = require('needle'),
@@ -32,10 +38,17 @@ bot.on('error', function(err) {
     console.log(err);
 });
 
+bot.on('invite', function(event) {
+    channels.push(event.channel);
+    channels[event.channel] = { running: false };
+    bot.join(event.channel);
+});
+
 bot.on('connected', function() {
     let db = new nedb(config.nedb);
     config.irc.channels.forEach( channel => {
         bot.join(channel);
+        channels[channel] = { running: false };
     });
     stream.startStream(db);
 });
@@ -93,6 +106,28 @@ ${colors.red(` ❤ ${favorites.toLocaleString('en-us')}`)}`);
             bot.say(to,message);
         }
     }
+}
+
+function postImage(to,from,prompt){
+    let url = config.ghetty.url,
+        data = {
+            image:{
+                file: path.join(__dirname,'images',to,'dalle.jpg'),
+                content_type: 'image/jpeg'
+            }
+        },
+        options = {
+            multipart:true,
+            json:true
+        };
+    needle.post(url, data, options, function(error, response, body) {
+        if (!error && response.statusCode == 200){
+            bot.say(to,`@${from} here you go: "${prompt}" ${body.href}`);
+        } else {
+            bot.say(to,`Ghetty error ${response.statusCode}: ${error}!.`);
+        }
+    });
+    channels[to].running = false;
 }
 
 exports.sayToChannel = function(channel,message) {
@@ -313,14 +348,15 @@ bot.on('message', function(event) {
                 bot.say(to,'Invalid command.');
         } else
         // get twitter.com or t.co link
-        if (message.match(/twitter\.com\/\w+\/status\/\d+/) || message.match(/twitter\.com\/i\/web\/status\/\d+/) || message.match(/t\.co\/\w+/)) {
+/*        if (message.match(/twitter\.com\/\w+\/status\/\d+/) || message.match(/twitter\.com\/i\/web\/status\/\d+/) || message.match(/t\.co\/\w+/)) {
             
 
             if (config.twitter && config.twitter.consumerKey && config.twitter.consumerSecret && config.twitter.token && config.twitter.token_secret) {
                 if (message.match(/t\.co\/\w+/)) {
                     // message contains a t.co link
                     message=`https://${message.match(/t\.co\/\w+/)[0]}`;
-                    needle({'url':message,'headers': {'User-Agent': 'request'}},function(err,response) {
+                    console.log(message);
+                    needle({'url':message,'headers': {'User-Agent': 'needle'}},function(err,response) {
                         message=response.request.uri.href;
                         if (message.match(/twitter\.com\/\w+\/status\/\d+/)) {
                             // it is a valid twitter status url
@@ -419,7 +455,8 @@ bot.on('message', function(event) {
                     
             } else // No auth data, ask user to authenticate bot
                     bot.say(to,'No auth data.');
-        } else
+        } else 
+        */
         // if message is .help
         if (message.match(/^\.help$/)) {
             bot.say(from,'Usage:');
@@ -428,6 +465,7 @@ bot.on('message', function(event) {
             setTimeout(function() { bot.say(from,'.following - show currently followed accounts in channel.');},500);
             setTimeout(function() { bot.say(from,'.follow @twitter_handle - follows the account in the channel.');},500);
             setTimeout(function() { bot.say(from,'.unfollow @twitter_handle - unfollows the account in the channel.');},500);
+            setTimeout(function() { bot.say(from,'.dalle <prompt> - request dall-e images from prompt');},500);
             setTimeout(function() { bot.say(from,'.help - this help message.');},500);
         } else
         if (message.match(/^\.bots$/)) {
@@ -499,12 +537,72 @@ bot.on('message', function(event) {
                 });
             } else // No auth data, ask user to authenticate bot
                 bot.say(to,'No auth data.');
+        } else
+        if (message.match(/\.dalle\s.+$/)) {
+            let
+                url = config.dalle.api_url,
+                prompt = message.slice(message.match(/\.dalle\s.+$/).index+7).trim();
+            // check if bot is not handling another call
+            if (!channels[to].running){
+                channels[to].running = true;
+                bot.say(to,`Generating from "${prompt}" prompt...`);
+                needle.post(url, {prompt: prompt},{json: true}, function(error, response) {
+                    if (!error && response.statusCode == 200){
+                        // save 9 images
+                        if (!fs.existsSync(path.join(__dirname,'images',to))){
+                            fs.mkdirSync(path.join(__dirname,'images',to), { recursive: true });
+                        }
+                        for (let i=0; i < response.body.images.length ; i++){
+                            let buffer = Buffer.from(response.body.images[i], "base64");
+                            fs.writeFileSync(path.join(__dirname,'images',to,`dall-e_result_${i}.jpg`), buffer);
+                        }
+                        const options_horizontal = {
+                            direction:"horizontal",
+                            color: 0x00000000,
+                            align: 'left', 
+                            offset: 5
+                            },
+                            options_vertical = {
+                                direction:"vertical",
+                                color: 0x00000000,
+                                align: 'left', 
+                                offset: 5
+                            };
+                        // join 9 images into a single 3x3 grid image
+                        joinImages.joinImages([path.join(__dirname,'images',to,'dall-e_result_0.jpg'), path.join(__dirname,'images',to,'dall-e_result_1.jpg'),path.join(__dirname,'images',to,'dall-e_result_2.jpg')],options_horizontal).then((img) => {
+                            img.toFile(path.join(__dirname,'images',to,'row1.jpg'));
+                            joinImages.joinImages([path.join(__dirname,'images',to,'dall-e_result_3.jpg'), path.join(__dirname,'images',to,'dall-e_result_4.jpg'),path.join(__dirname,'images',to,'dall-e_result_5.jpg')],options_horizontal).then((img) => {
+                                img.toFile(path.join(__dirname,'images',to,'row2.jpg'));
+                                joinImages.joinImages([path.join(__dirname,'images',to,'dall-e_result_6.jpg'), path.join(__dirname,'images',to,'dall-e_result_7.jpg'),path.join(__dirname,'images',to,'dall-e_result_8.jpg')],options_horizontal).then((img) => {
+                                    img.toFile(path.join(__dirname,'images',to,'row3.jpg'));
+                                    setTimeout(function(){
+                                        joinImages.joinImages([path.join(__dirname,'images',to,'row1.jpg'),path.join(__dirname,'images',to,'row2.jpg'),path.join(__dirname,'images',to,'row3.jpg')],options_vertical).then((img) => {
+                                            img.toFile(path.join(__dirname,'images',to,'dalle.jpg'));
+                                            setTimeout(function(){postImage(to,from,prompt)},500);
+                                        });
+                                    },500);
+                                });
+                            });
+                        });
+                    } else {
+                        if (response.statusCode == 524){
+                            bot.say(to,`@${from} Dall-E Service is too Busy. Please try again later...`);
+                        } else {
+                            bot.say(to,`Dall-E Error ${response.statusCode}: ${response.statusMessage}`);
+                        }
+                        channels[to].running = false;
+                    }
+                });
+            } else {
+                bot.say(to,`@${from} please wait for the current Dall-E request to complete.`);
+            }
         }
     }
 });
 
 bot.on('registered', function (){
-    bot.say('nickserv','identify '+ pass);
+    bot.say('nickserv','identify '+ password);
 })
 
-bot.connect({host,port,tls,nick});
+bot.connect({host,port,tls,nick,username,gecos,password});
+
