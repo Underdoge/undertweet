2671da107708850d9343d7f4424e0ade4bd29529
Eduardo Chapa <eduardo.chapa@gmail.com>
2022-11-07 17:38:16 -0600

    ok

--- irc.js
+++ irc.js
@@ -8,7 +8,7 @@ const
     IRC = require('irc-framework'),
     colors = require('irc-colors'),
     config = require('./config'),
-    stream = require('./streamv2'),
+    //stream = require('./streamv2'),
     path = require('path'),
     nedb = require('nedb'),
     host = config.irc.host,
@@ -472,7 +472,7 @@ bot.on('connected', async function() {
     } else {
         await joinChannels(db);
     }
-    stream.startStream(db);
+    //stream.startStream(db);
 });
 
 bot.on('message', async function(event) {
