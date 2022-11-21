0e8fddb609537bd9c7e153d06891977d7949c0f0
Eduardo Chapa <eduardo.chapa@gmail.com>
2022-11-20 18:49:45 -0600

    removed console.log

--- irc.js
+++ irc.js
@@ -1178,7 +1178,6 @@ bot.on('message', async function(event) {
                     } else {
                         title = message.slice(message.match(/\.imdb\s.+$/).index+6).trim().toLocaleLowerCase('en-us').normalize("NFD").replace(/[\u0300-\u036f]/g, "");
                     }
-                    console.log(`title: ${title}`)
                     let yearSearch = false;
                     imdbquery = "https://www.imdb.com/search/title/?title=" + title.replaceAll(" ","+");
                     if (message.match(/-m/)) {
@@ -1192,7 +1191,6 @@ bot.on('message', async function(event) {
                         imdbquery += "&release_date=" + message.slice(message.match(/[1-2][0,8,9]([0-9]){2}/).index,message.match(/[1-2][0,8,9]([0-9]){2}/).index+4) + "-01-01,";
                     }
                     // check if bot is not handling another call
-                    console.log(imdbquery);
                     needle.defaults({user_agent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52'});
                     needle.get(imdbquery, { headers: { "Accept-Language": "en-US" }}, function(err, res, body) {
                         const $ = cheerio.load(body);
